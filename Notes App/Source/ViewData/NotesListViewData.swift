65a0cefe4beede2f098375f99127a68d73e06e04
Simran Preet Singh <simran5590@gmail.com>
2023-07-11 07:18:08 -0700

    Added searching for notes using the new Observable framework

--- Notes App/Source/ViewData/NotesListViewData.swift
+++ Notes App/Source/ViewData/NotesListViewData.swift
@@ -11,6 +11,7 @@ import Observation
 @Observable
 final class NotesListViewData {
     
+    private var originalNotes: [Note] = []
     var notes: [Note] = []
     var folderName: String = ""
     var searchText: String = ""
@@ -18,8 +19,23 @@ final class NotesListViewData {
         return "\(notes.count)"
     }
     
+    
     init(folder: Folder) {
         self.notes = folder.notes
+        self.originalNotes = self.notes
         self.folderName = folder.name
+        observeProperties()
+    }
+    
+    private func observeProperties() {
+        withObservationTracking {
+            let _ = searchText
+        } onChange: { [weak self] in
+            guard let self else { return }
+            observeProperties()
+            notes = searchText.count >= 1
+            ? originalNotes.filter { $0.title.lowercased().contains(self.searchText.lowercased()) ||  $0.details.lowercased().contains(self.searchText.lowercased())}
+            : originalNotes
+        }
     }
 }
